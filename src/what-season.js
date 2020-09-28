15a4697ea5f9ca4590b01c8cddcd5091437d7ac8
Constantine Kolobov <kolobovconstantine@gmail.com>
2020-09-28 20:56:42 +0300

    feat: solve what-season task

--- src/what-season.js
+++ src/what-season.js
@@ -1,6 +1,31 @@
 const CustomError = require("../extensions/custom-error");
 
-module.exports = function getSeason(/* date */) {
-  throw new CustomError('Not implemented');
-  // remove line with error and write your code here
+const getSeasonFromMonth = (monthNumber) => {
+  if (typeof monthNumber !== 'number' || monthNumber < 0 || monthNumber > 11) {
+    throw new Error('Wrong month!');
+  }
+  const seasons = ['winter','winter','spring','spring','spring','summer','summer','summer','autumn','autumn','autumn','winter'];
+  return seasons[monthNumber];
+}
+
+const isDateObj = (obj) => {
+  let result = false;
+  if (obj instanceof Date) {
+    if (Object.keys(obj).length === 0) {
+      result = true;
+    }
+  }
+  return result;
+}
+
+module.exports = function getSeason(date) {
+  let result = 'Unable to determine the time of year!';
+  if (date) {
+    if (isDateObj(date)) {
+      result = getSeasonFromMonth(date.getMonth());
+    } else {
+      throw new Error('passed argument is not instance of Date')
+    }
+  }
+  return result;
 };
