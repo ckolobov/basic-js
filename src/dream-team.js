64eb78cd6fb9eb489774019faff5226d80bba663
Constantine Kolobov <kolobovconstantine@gmail.com>
2020-09-28 19:57:19 +0300

    feat: solve dream-team task

--- src/dream-team.js
+++ src/dream-team.js
@@ -1,6 +1,14 @@
 const CustomError = require("../extensions/custom-error");
 
-module.exports = function createDreamTeam(/* members */) {
-  throw new CustomError('Not implemented');
-  // remove line with error and write your code here
+module.exports = function createDreamTeam(members) {
+  let result = false;
+  if (Array.isArray(members)) {
+    result = members.reduce((acc, val) => {
+      if (typeof val === 'string') {
+        acc.push(val.trim()[0].toUpperCase());
+      }
+      return acc;
+    }, []).sort().join('');
+  }
+  return result;
 };
